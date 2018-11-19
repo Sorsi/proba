849532c14b3fbd19b810d5e53627ed013442a089
Sorsi <orsolya.suto.so@gmail.com>
2018-11-19 19:38:11 +0100

    try to separate contact and about pages

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 
 class About extends Component {
   render() {
-    return(
-      <div>
-        <h1>
-          About Page
-        </h1>
+    return (
+      <div className="about-container">
+        <h2>Orsolya Sütő</h2>
+        <img
+          id="profile-pic"
+          src="Sorsi-about.png"
+          alt="profile"
+          style={{ height: '250px' }}
+        />
+        <p id="intro">
+          Welcome on my website! From February of 2018 I'm working on my webdeveloper skills.
+          Watched a lot of online videos and courses (Youtube, Udemy, Codeacademy...)
+          On 12th October 2018 graduated at Green Fox Academy and got my certification from software development.
+          Now I'm a junior Full Stack Web Developer using MERN (MongoDB, Express, React, Node.js),
+          Redux, RESTful APIs, MySQL/MongoDB, Lodash, Express, Bootstrap, Ant Design, SASS, LESS
+            </p>
       </div>
     )
   }
