49e0e132ec4ddc0903f9c55ff96f0c80bf0594ea
Sorsi <orsolya.suto.so@gmail.com>
2018-11-19 19:23:45 +0100

    hide about me

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <Layout >
           <Header className="header-color" title={<Link style={{textDecoration:'none', color: 'white'}} to="/">Sorsi</Link>} scroll>
               <Navigation>
-                <Link to="/about">About me</Link>
+                {/* <Link to="/about">About me</Link> */}
                 <Link to="/resume">Resume</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
@@ -20,7 +20,7 @@ class App extends Component {
             </Header>
             <Drawer title={<Link style={{textDecoration:'none', color: 'orange'}} to="/">Sorsi</Link>}>
               <Navigation>
-                <Link to="/about">About me</Link>
+                {/* <Link to="/about">About me</Link> */}
                 <Link to="/resume">Resume</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
