294230c13c34d2ca38947439c1fcccd807f7676d
Orsolya <orsolya.suto@ericsson.com>
2020-07-01 19:32:14 +0200

    updating projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,87 +1,199 @@
-import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, CardMenu, IconButton } from 'react-mdl';
+import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardText,
+  CardActions,
+  CardMenu,
+  IconButton,
+} from "react-mdl";
+import quizUp from "../images/quizUp.png";
+import weatherApp from "../images/weatherApp.png";
+import roboFriends from "../images/roboFriends.png";
 
 class Projects extends Component {
   state = {
     activeTab: 0,
-  }
+  };
 
   toggleCategories() {
     if (this.state.activeTab === 0) {
       return (
         <div className="projects-grid">
-          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-            <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2000px-React-icon.svg.png) center / cover' }}>
-              React Project #1
-         </CardTitle>
-            <CardText>
-              blblblblbblblbllskkasdklklasdkjldskljdskljdsffkjldskljdskljdskljdkjldskjldskjldslkjésdfa
-             kdsfjlksdjfopeirjfklndjkhsukdhgkjdfhgueoifhgg
-             dfgjoiefogjoegjoufjgfdjoigdfjoigjoiffiodfoijdfiojfd
-         </CardText>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#000",
+                height: "176px",
+                backgroundImage: `url(${quizUp})`,
+              }}
+            ></CardTitle>
+            <CardText>CoffeeQuiz</CardText>
             <CardActions border>
-              <button>GitHub</button>
-              <button>CodePen</button>
-              <button>LiveDemo</button>
+              <a
+                className="button-ancestor"
+                href="https://github.com/Sorsi/CoffeeQuiz"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                GitHub
+              </a>
+              <a
+                className="button-ancestor"
+                href={quizUp}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Show a pic
+              </a>
             </CardActions>
-            <CardMenu style={{ color: '#fff' }}>
+            <CardMenu style={{ color: "#fff" }}>
               <IconButton name="share" />
             </CardMenu>
           </Card>
-          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-            <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2000px-React-icon.svg.png) center / cover' }}>
-              React Project #2
-         </CardTitle>
-            <CardText>
-              blblblblbblblbllskkasdklklasdkjldskljdskljdsffkjldskljdskljdskljdkjldskjldskjldslkjésdfa
-             kdsfjlksdjfopeirjfklndjkhsukdhgkjdfhgueoifhgg
-             dfgjoiefogjoegjoufjgfdjoigdfjoigjoiffiodfoijdfiojfd
-         </CardText>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#000",
+                height: "176px",
+                background:
+                  "url(https://frontendlabepam.github.io/FL5/angular/assets/img/angular-js-logo.png) center / cover",
+              }}
+            ></CardTitle>
+            <CardText>Filming Locations</CardText>
             <CardActions border>
-              <button>GitHub</button>
-              <button>CodePen</button>
-              <button>LiveDemo</button>
+              <a
+                className="button-ancestor"
+                href="https://github.com/Sorsi/Movies"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                GitHub
+              </a>
+              <a className="button-ancestor">Show a pic</a>
             </CardActions>
-            <CardMenu style={{ color: '#fff' }}>
+            <CardMenu style={{ color: "#fff" }}>
               <IconButton name="share" />
             </CardMenu>
           </Card>
-          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-            <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2000px-React-icon.svg.png) center / cover' }}>
-              React Project #3
-         </CardTitle>
-            <CardText>
-              blblblblbblblbllskkasdklklasdkjldskljdskljdsffkjldskljdskljdskljdkjldskjldskjldslkjésdfa
-             kdsfjlksdjfopeirjfklndjkhsukdhgkjdfhgueoifhgg
-             dfgjoiefogjoegjoufjgfdjoigdfjoigjoiffiodfoijdfiojfd
-         </CardText>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#000",
+                height: "176px",
+                background:
+                  "url(https://frontendlabepam.github.io/FL5/angular/assets/img/angular-js-logo.png) center / cover",
+              }}
+            ></CardTitle>
+            <CardText>### Angular Project #3 ###</CardText>
             <CardActions border>
-              <button>GitHub</button>
-              <button>CodePen</button>
-              <button>LiveDemo</button>
+              <a className="button-ancestor">GitHub</a>
+              <a className="button-ancestor">Show a pic</a>
             </CardActions>
-            <CardMenu style={{ color: '#fff' }}>
+            <CardMenu style={{ color: "#fff" }}>
               <IconButton name="share" />
             </CardMenu>
           </Card>
         </div>
-      )
+      );
     } else if (this.state.activeTab === 1) {
       return (
-        <div>
-           <h1>
-             This is mongoDB
-           </h1>
+        <div className="projects-grid">
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#000",
+                height: "176px",
+                backgroundImage: `url(${weatherApp})`,
+              }}
+            ></CardTitle>
+            <CardText>Weather App</CardText>
+            <CardActions border>
+              <a
+                className="button-ancestor"
+                href="https://github.com/Sorsi/PlanetApps/tree/master/weather-app"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                GitHub
+              </a>
+              <a
+                className="button-ancestor"
+                href={weatherApp}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Show a pic
+              </a>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#000",
+                height: "176px",
+                background:
+                  `url(${roboFriends})  center / cover`,
+              }}
+            >
+            </CardTitle>
+            <CardText>RoboFriends</CardText>
+            <CardActions border>
+            <a
+                className="button-ancestor"
+                href="https://github.com/Sorsi/PlanetApps/tree/master/robofriends"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                GitHub
+              </a>
+              <a
+                className="button-ancestor"
+                href={roboFriends}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Show a pic
+              </a>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#000",
+                height: "176px",
+                background:
+                  "url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2000px-React-icon.svg.png) center / cover",
+              }}
+            >
+            </CardTitle>
+            <CardText>### React Project 3 ###</CardText>
+            <CardActions border>
+              <button>GitHub</button>
+              <button>CodePen</button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
         </div>
-      )
+      );
     } else if (this.state.activeTab === 2) {
       return (
         <div>
-          <h1>
-            This is Angular
-            </h1>
+          <h1>Coming soon...</h1>
         </div>
-      )
+      );
     }
   }
 
@@ -90,10 +202,12 @@ class Projects extends Component {
       <div className="category-tabs">
         <Tabs
           activeTab={this.state.activeTab}
-          onChange={(tabID) => this.setState({ activeTab: tabID })} ripple>
-          <Tab>React</Tab>
-          <Tab>MongoDB</Tab>
+          onChange={(tabID) => this.setState({ activeTab: tabID })}
+          ripple
+        >
           <Tab>Angular</Tab>
+          <Tab>React</Tab>
+          <Tab>Vue.js</Tab>
         </Tabs>
         <Grid>
           <Cell col={12}>
@@ -101,7 +215,7 @@ class Projects extends Component {
           </Cell>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
